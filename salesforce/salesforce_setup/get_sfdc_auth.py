7c3144b0adc5895cdbd84efe6ca0e6b2e3069551
joelws <joel.w.simmons@gmail.com>
2022-08-14 19:38:22 -0500

    initial commit

---
+++ salesforce/salesforce_setup/get_sfdc_auth.py
@@ -0,0 +1,29 @@
+import json
+from cryptography.fernet import Fernet 
+from salesforce_setup.setup_sfdc_creds import load_key
+
+def decrpt_sfdc_cred(encrypted_message):
+    '''
+    Decrypts encrypted SFDC credential as a string 
+    '''
+    # Import fernet key 
+    key = load_key()
+    # Init fernet 
+    f = Fernet(key)
+    # Encode string from JSON for fernet 
+    encrypted_message = encrypted_message.encode()
+    # Decrypt message 
+    decrypted_message =  f.decrypt(encrypted_message)
+    # return decrypted message as str 
+    return decrypted_message.decode()
+
+# Open SFDC Ecrypted Auth JSON 
+with open('sfdc_auth.json') as f:
+    sfdc_auth_encrypted = json.load(f)
+
+# Create SFDC Auth Obj with unencrypted keys 
+sfdc_auth = {
+    'sf_username' : decrpt_sfdc_cred(sfdc_auth_encrypted['sf_username']),
+    'sf_password' : decrpt_sfdc_cred(sfdc_auth_encrypted['sf_password']),
+    'sf_token' : decrpt_sfdc_cred(sfdc_auth_encrypted['sf_token'])
+}
---
+++ salesforce/salesforce_setup/salesforce_setup.py
@@ -0,0 +1,56 @@
+import simple_salesforce
+import pandas as pd
+import os 
+
+# Import SFDC Auth dict 
+try: 
+    from salesforce_setup.get_sfdc_auth import sfdc_auth
+except:
+    from salesforce_setup.setup_sfdc_creds import encrypt_sfdc_creds
+    # Run Encrypt Salesforce Creds Function to init creds 
+    encrypt_sfdc_creds()
+    from salesforce_setup.get_sfdc_auth import sfdc_auth
+ 
+class MySalesforce:
+ 
+    # Create Salesforce auth client 
+    sf = simple_salesforce.Salesforce(
+    username=sfdc_auth['sf_username'],
+    password=sfdc_auth['sf_password'],
+    security_token=sfdc_auth['sf_token']
+    ) 
+ 
+    # Salesforce query function 
+    def sfdc_query(query,is_sql_file=False):
+        '''
+        Queries salesforce using the simple_salesforce package
+        Params: 
+        - query: Type - string or filepath | SQL Query filepath or text input defined in a variable
+        - is_sql_file: Type - Bool | mark true if it's a file, so that it will open. Defualt = FALSE in order to accept a string. 
+        '''
+        sf = my_salesforce.sf
+ 
+        if is_sql_file == True:
+ 
+            sfdc_sql_query = open(query).read()
+ 
+            sf_object_data = sf.query_all(sfdc_sql_query)
+ 
+            sf_object_df = pd.DataFrame(sf_object_data['records'])
+ 
+            df = sf_object_df.drop('attributes', axis='columns')
+ 
+            return df
+ 
+        else: 
+ 
+            sfdc_sql_query = query
+ 
+            sf_object_data = sf.query_all(sfdc_sql_query)
+ 
+            sf_object_df = pd.DataFrame(sf_object_data['records'])
+ 
+            df = sf_object_df.drop('attributes', axis='columns')
+            
+            return df
+
